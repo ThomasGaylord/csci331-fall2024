bff86a0be2ceed8c0e972e380c654e8f9f88bcce
ThomasGaylord <153327353+ThomasGaylord@users.noreply.github.com>
2024-11-02 03:41:42 -0600

    initial commit

---
+++ WebDev/first-nextjs/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Counter from '../components/Counter';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className={styles.title}>
+          Done by Thomas Gaylord using <a href="https://nextjs.org">Next.js.</a>
+      </h1>
+      <Counter />
+        
+    </div>
+  );
+}

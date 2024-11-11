9411122eed248c6f995e80fa9ac1cfe993529096
ThomasGaylord <153327353+ThomasGaylord@users.noreply.github.com>
2024-11-10 18:30:29 -0700

    Delete first-nextjs directory

--- first-nextjs/components/GitHub.js
+++
@@ -1,8 +0,0 @@
-export default function GitHub(){
-    return(
-        <div>
-            <p>Link to my Github: <a href="https://github.com/ThomasGaylord/csci331-fall2024/tree/first">https://github.com/ThomasGaylord/csci331-fall2024/tree/first</a> </p>
-            <p>I'm doing my project on how docker can be used to implement a SQL Database. This is going to be done by setting up a SQL database in our local system in class, then showing how quick a pre-created docker container can create the same database on our local system. If more time is necessary we can also show how major changes can be quickly propogated to other systems by copying the setup from one database to a docker file then sharing it and running it.</p>
-        </div>
-    );
-}
